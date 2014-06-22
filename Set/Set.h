03a74927aaea4bbe139429bdac0c540aff81b6fb
Rob Rix <rob.rix@github.com>
2014-06-22 14:25:38 -0400

    Initial Commit

---
+++ Set/Set.h
@@ -0,0 +1,19 @@
+//
+//  Set.h
+//  Set
+//
+//  Created by Rob Rix on 2014-06-22.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Set.
+FOUNDATION_EXPORT double SetVersionNumber;
+
+//! Project version string for Set.
+FOUNDATION_EXPORT const unsigned char SetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Set/PublicHeader.h>
+
+
---
+++ SetTests/SetTests.swift
@@ -0,0 +1,35 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by Rob Rix on 2014-06-22.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+import XCTest
+
+class SetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
