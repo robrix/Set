1a94e0be0c0e2362180417de7dcb79015d5058e4
Rob Rix <rob.rix@github.com>
2014-06-28 10:58:14 -0400

    Test unions.

--- SetTests/SetTests.swift
+++ SetTests/SetTests.swift
@@ -4,7 +4,14 @@ import XCTest
 import Set
 
 class SetTests: XCTestCase {
-	func testUnion() {
+	func testUnionAddsElementsFromBothOperands() {
+		XCTAssert(Set([1, 2, 3, 4]) + Set([3, 4, 5]) == Set([1, 2, 3, 4, 5]))
+	}
+	
+	func testUnionAssignmentModifiesInPlace() {
+		var c: Set<Int> = [1, 2, 3]
+		c += Set([3, 4, 5])
 		
+		XCTAssert(c == Set([1, 2, 3, 4, 5]))
 	}
 }
