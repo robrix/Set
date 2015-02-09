be29a53788b8cd76b8c02d620e16462b1a1ce576
Rob Rix <rob.rix@github.com>
2015-02-08 22:08:17 -0500

    Factor out joining the lazily-transformed elements of the sequences.

--- Set/Printing.swift
+++ Set/Printing.swift
@@ -2,14 +2,19 @@
 
 /// Describes a sequence as a set.
 internal func describe<S: SequenceType>(sequence: S) -> String {
-	return wrapDescription(join(", ", lazy(sequence).map(toString)))
+	return mapDescription(sequence, toString)
 }
 
 /// Debug-describes a sequence as a set.
 internal func debugDescribe<S: SequenceType>(sequence: S) -> String {
-	return wrapDescription(join(", ", lazy(sequence).map(toDebugString)))
+	return mapDescription(sequence, toDebugString)
 }
 
+
+/// Maps the elements of `sequence` with `transform` and formats them as a set.
+private func mapDescription<S: SequenceType>(sequence: S, transform: S.Generator.Element -> String) -> String {
+	return wrapDescription(join(", ", lazy(sequence).map(transform)))
+}
 
 /// Wraps a string appropriately for formatting as a set.
 private func wrapDescription(description: String) -> String {
